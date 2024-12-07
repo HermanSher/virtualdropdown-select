0cd472ff8e4c97daf0b85e05f8819db53640fcee
HermanSher <hermansher1111@gmail.com>
2024-12-07 20:03:28 +0530

    Update README.md with props table and usage

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: 'src/VirtualDropdown.jsx',  // Entry point for your component
+      name: 'VirtualDropdownSelect',     // Global variable name in UMD build
+      fileName: (format) => `virtualdropdown-select.${format}.js`, // Output file name
+    },
+    rollupOptions: {
+      // Ensure that external libraries like React are not bundled into the package
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
